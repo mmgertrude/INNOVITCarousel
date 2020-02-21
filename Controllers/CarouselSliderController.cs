1fd26914f5666b052504041cc27e3818c5d593e5
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2020-02-21 18:44:47 +0100

    Add project files.

---
+++ Controllers/CarouselSliderController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using INNOVITCarousel.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace INNOVITCarousel.Controllers
+{
+    public class CarouselSliderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /*
+        [HttpGet]
+        public ActionResult UploadSliderImage()
+        {
+            List<CarouselSlider> sliderimages = new List<CarouselSlider>();
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllSliderImage", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    CarouselSlider slider = new CarouselSlider();
+                    slider.ID = Convert.ToInt32(rdr["ID"]);
+                    slider.Name = rdr["Name"].ToString();
+                    slider.FileSize = Convert.ToInt32(rdr["FileSize"]);
+                    slider.FilePath = rdr["FilePath"].ToString();
+
+                    sliderimages.Add(slider);
+                }
+            }
+            return View(sliderimages);
+        }*/
+
+
+
+    }
+
+
+    
+
+
+
+
+
+
+
+}
---
+++ Models/CarouselSlider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INNOVITCarousel.Models
+{
+    public class CarouselSlider
+    {
+
+        //Write field and property of CarouselSlider as we have in the database table.
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> FileSize { get; set; }
+        public string FilePath { get; set; }
+    }
+}
